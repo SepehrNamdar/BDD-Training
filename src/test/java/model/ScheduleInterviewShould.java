ce75b13f5d3062a8bdbc3c796466dff5374abd4c
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2019-07-10 16:55:31 +0200

    implement the first scenario

--- src/test/java/model/ScheduleInterviewShould.java
+++ src/test/java/model/ScheduleInterviewShould.java
@@ -1,33 +1,41 @@
 package model;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import use_case.ScheduleInterview;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.assertEquals;
 
 public class ScheduleInterviewShould {
+    private final LocalDateTime TODAY = LocalDateTime.now();
+
+    private Candidate julia;
+    private Recruiter david;
+    private ScheduleInterview scheduler;
+
     @Given("Julia who is a Java developer and is available today")
     public void julia_who_is_a_java_developer_and_is_available_today() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        julia = new Candidate("Julia", "Java", TODAY);
     }
 
     @And("^David who is a Java recruiter and is available today$")
     public void davidWhoIsAJavaRecruiterAndIsAvailableToday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        david = new Recruiter("David", "Java", TODAY);
     }
 
     @When("^I try to schedule an interview for Julia today$")
-    public void iTryToScheduleAnInterviewForJuliaToday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void iTryToScheduleAnInterviewForJulia() {
+        scheduler = new ScheduleInterview(julia, david);
+        scheduler.planInterview();
     }
 
     @Then("^an interview is scheduled for Julia and Thomas today to meet each other$")
     public void anInterviewIsScheduledForJuliaAndThomasTodayToMeetEachOther() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        Interview interviewTodayBetweenJuliaAndDavid = new Interview(TODAY, julia.getName(), david.getName());
+        assertEquals(interviewTodayBetweenJuliaAndDavid, scheduler.getInterview());
     }
 }
